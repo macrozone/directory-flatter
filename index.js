41d83bf90f688f55a7f4ba48230df2236abbb39f
Marco <macrozone@gmail.com>
2018-12-09 22:28:12 +0100

    exclude hidden files

--- index.js
+++ index.js
@@ -11,31 +11,34 @@ klaw(input)
       const { path, stats } = item
 
       const [fileName, parentFolder] = path.split('/').reverse()
-      //console.log(fileName, parentFolder)
-      const fileMoment = moment(stats.birthtimeMs || stats.mtimeMs)
-      const date = fileMoment.format('YYYY-MM-DD')
-      const month = fileMoment.format('MM')
-      const year = fileMoment.format('YYYY')
-      const folderNameToCreate = year + '/' + month
-      const filenameToCreate = parentFolder + ' ' + fileName
+      if (!fileName.startsWith('.')) {
+        //console.log(fileName, parentFolder)
+        const fileMoment = moment(stats.birthtimeMs || stats.mtimeMs)
+        const date = fileMoment.format('YYYY-MM-DD')
+        const month = fileMoment.format('MM')
+        const year = fileMoment.format('YYYY')
+        const folderNameToCreate = year + '/' + month
+        const filenameToCreate = parentFolder + ' ' + fileName
 
-      const target = output + '/' + folderNameToCreate + '/' + filenameToCreate
-      fs.copy(
-        path,
-        target,
-        {
-          dereference: true,
-          preserveTimestamps: true
-        },
-        err => {
-          if (err) {
-            console.error(
-              'error copying file from ' + path + ' to ' + target,
-              err
-            )
+        const target =
+          output + '/' + folderNameToCreate + '/' + filenameToCreate
+        fs.copy(
+          path,
+          target,
+          {
+            dereference: true,
+            preserveTimestamps: true
+          },
+          err => {
+            if (err) {
+              console.error(
+                'error copying file from ' + path + ' to ' + target,
+                err
+              )
+            }
           }
-        }
-      )
+        )
+      }
     }
   })
   .on('end', () => {
